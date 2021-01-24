6edfe753a329fd591e2eee76965bc1445340c58e
Philipp Sonntag <sonntag.philipp@outlook.de>
2021-01-24 22:39:58 +0100

    Removed example code.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
 </template>
 
 <script setup>
-import Navigation from './components/Navigation.vue'
+  import Navigation from './components/Navigation.vue'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 </style>
